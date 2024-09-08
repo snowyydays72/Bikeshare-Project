01464714ecea1fb41279322c8f4b6e90ebeb9919
Hunter <hunterfranklin001@gmail.com>
2024-09-08 17:31:41 +1000

    Updated bikeshare.py to add commenting to the data filters

--- bikeshare.py
+++ bikeshare.py
@@ -5,7 +5,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+#Section to ask which city, month and day to display data
 def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     
